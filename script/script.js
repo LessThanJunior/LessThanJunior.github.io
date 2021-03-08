ac80259fd92e280008fb76d7473f9ce34d14771a
LessThanJunior <nik.dymarchuk@gmail.com>
2021-03-08 18:37:25 +0200

    Add modal image

--- script/script.js
+++ script/script.js
@@ -19,6 +19,67 @@ var scrolltoservices = document.getElementsByClassName("icon__container");
 var scroll_to_services = scrolltoservices[0]
 var testimonials = document.querySelector("a.about__testimonials")
 var testContainer = document.querySelector("div.testimonials__container")
+var modal = document.querySelector(".modal")
+var myModal = document.querySelector(".myModal")
+var img1 = document.getElementById("myImg1");
+var img2 = document.getElementById("myImg2");
+var img3 = document.getElementById("myImg3");
+var img4 = document.getElementById("myImg4");
+var img5 = document.getElementById("myImg5");
+var img6 = document.getElementById("myImg6");
+var img7 = document.getElementById("myImg7");
+var img8 = document.getElementById("myImg8");
+
+img1.onclick = function(){
+	modal.style.display="flex";
+	myModal.src = this.src;
+	document.body.classList.add("lock");
+}
+
+img2.onclick = function(){
+	modal.style.display="flex";
+	myModal.src = this.src
+	document.body.classList.add("lock");
+}
+
+img3.onclick = function(){
+	modal.style.display="flex";
+	myModal.src = this.src
+	document.body.classList.add("lock");
+}
+
+img4.onclick = function(){
+	modal.style.display="flex";
+	myModal.src = this.src
+	document.body.classList.add("lock");
+}
+
+img5.onclick = function(){
+	modal.style.display="flex";
+	myModal.src = this.src
+	document.body.classList.add("lock");
+}
+
+img6.onclick = function(){
+	modal.style.display="flex";
+	myModal.src = this.src
+	document.body.classList.add("lock");
+}
+
+img7.onclick = function(){
+	modal.style.display="flex";
+	myModal.src = this.src
+	document.body.classList.add("lock");
+}
+
+img8.onclick = function(){
+	modal.style.display="flex";
+	myModal.src = this.src
+	document.body.classList.add("lock");
+}
+
+
+
 
 console.log(videoel)
 
